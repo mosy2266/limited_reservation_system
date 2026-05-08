264b51fee7a629ec8ba64f30ecba7438c275babf
Yun <mosy2266@naver.com>
2026-05-08 19:56:15 +0900

    chore: AGENTS.md 가이드라인 추가

---
+++ src/main/java/reservation/limited/LimitedApplication.java
@@ -0,0 +1,13 @@
+package reservation.limited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/reservation/limited/LimitedApplicationTests.java
@@ -0,0 +1,13 @@
+package reservation.limited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
