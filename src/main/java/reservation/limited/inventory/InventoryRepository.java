6b157e0fcacdc0d3ecf58424f43d48a660d78d6f
Yun <mosy2266@naver.com>
2026-05-11 17:38:37 +0900

    feat: 상품 및 재고 도메인 예약 검증 로직 추가 (#3)

--- src/main/java/reservation/limited/inventory/InventoryRepository.java
+++ src/main/java/reservation/limited/inventory/InventoryRepository.java
@@ -1,6 +1,13 @@
 package reservation.limited.inventory;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Inventory> findWithLockByProductId(Long productId);
 }
