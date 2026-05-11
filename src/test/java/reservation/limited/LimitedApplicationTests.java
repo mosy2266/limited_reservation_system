ddab60c49226b2ff147ea37b5c3be91dae34ef75
Yun <mosy2266@naver.com>
2026-05-11 17:24:44 +0900

    chore: Redis Cluster 및 MySQL 관련 설정 추가 (#3)

--- src/test/java/reservation/limited/LimitedApplicationTests.java
+++ src/test/java/reservation/limited/LimitedApplicationTests.java
@@ -2,8 +2,16 @@ package reservation.limited;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:limited-context-test;MODE=MySQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class LimitedApplicationTests {
 
     @Test
